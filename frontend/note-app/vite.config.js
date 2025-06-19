f956ee4654679f7c24df9208a52f807b690a9edf
frothybike <frothybike8@gmail.com>
2025-06-19 23:13:44 +0900

    create vite project

--- frontend/note-app/vite.config.js
+++ frontend/note-app/vite.config.js
@@ -1,13 +1,6 @@
-import * as path from 'path'
 export default {
-  root: path.resolve(__dirname, 'src'),
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
-  server: {
-    port: 8080,
-    hot: true
+  root: 'src',
+  build: {
+    outDir: '../dist'
   }
 }
