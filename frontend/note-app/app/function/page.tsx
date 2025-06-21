59d25182010a7090ec60abb2544088e5c176b992
frothybike <frothybike8@gmail.com>
2025-06-21 14:10:57 +0900

    create lp

---
+++ frontend/note-app/app/function/page.tsx
@@ -0,0 +1,12 @@
+export default function Function() {
+  return (
+    <main className="text-gray-600 body-font">
+      <div className="mx-auto p-5 container">
+        <div className="py-5 text-center">
+          <h1 className="mb-5 text-4xl font-extrabold text-gray-900">機能</h1>
+          <p className="mb-10">機能を紹介するページです。</p>
+        </div>
+      </div>
+    </main>
+  );
+}
