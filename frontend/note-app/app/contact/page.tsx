9eb0254190dd07499fb486ab342357f695cac289
frothybike <frothybike8@gmail.com>
2025-06-22 19:24:52 +0900

    fix form page

--- frontend/note-app/app/contact/page.tsx
+++ frontend/note-app/app/contact/page.tsx
@@ -4,7 +4,6 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import Head from "next/head";
-import { useState } from "react";
 
 type FormData = {
   subject: string;
