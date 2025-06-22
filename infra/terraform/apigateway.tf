0d0e8b18edd2335fb961f6ed23cc3e999aef09ab
frothybike <frothybike8@gmail.com>
2025-06-22 20:20:54 +0900

    fix api gateway

--- infra/terraform/apigateway.tf
+++ infra/terraform/apigateway.tf
@@ -83,3 +83,55 @@ resource "aws_api_gateway_integration_response" "post_response" {
     aws_api_gateway_method_response.post_response
   ]
 }
+
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.send_mail_api.id
+  resource_id   = aws_api_gateway_resource.send_mail_api_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id   = aws_api_gateway_rest_api.send_mail_api.id
+  resource_id   = aws_api_gateway_resource.send_mail_api_resource.id
+  http_method = "OPTIONS"
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = <<EOF
+{
+  "statusCode": 200
+}
+EOF
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_response" {
+  rest_api_id   = aws_api_gateway_rest_api.send_mail_api.id
+  resource_id   = aws_api_gateway_resource.send_mail_api_resource.id
+  http_method = "OPTIONS"
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'https://www.namabanana.com'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+  }
+}
+
+resource "aws_api_gateway_method_response" "options_response" {
+  rest_api_id   = aws_api_gateway_rest_api.send_mail_api.id
+  resource_id   = aws_api_gateway_resource.send_mail_api_resource.id
+  http_method = "OPTIONS"
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+  }
+}
